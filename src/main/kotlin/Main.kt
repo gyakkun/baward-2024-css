f870646da68f8e4312e78a3a32bd5f59e671173f
gyakkun <xiaowufeixia@gmail.com>
2025-01-27 15:29:29 +0800

    HEAD!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,10 @@ package moe.nyamori.test
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.head
+import io.javalin.http.Context
 import io.javalin.http.util.NaiveRateLimit
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.net.http.HttpClient
@@ -24,39 +27,43 @@ fun main() {
     val app = Javalin.create { config ->
         config.useVirtualThreads = false
         config.router.apiBuilder {
-            get("/health") { ctx ->
-                NaiveRateLimit.requestPerTimeUnit(ctx, 20, TimeUnit.MINUTES)
-                val req = HttpRequest.newBuilder()
-                    .uri(URI.create(cssUrl))
-                    .header("User-Agent", ua)
-                    .build()
-
-                val resFut = jdkHttpClient.sendAsync(req, HttpResponse.BodyHandlers.ofString())
-                runCatching {
-                    val res = resFut.get(5, TimeUnit.SECONDS)
-                    if (res.body().contains("吃掉了")) {
-                        ctx.result("not found yet")
-                        ctx.status(200)
-                        return@get
-                    } else if (res.headers().allValues("content-type").any { it.equals("text/css", true) }) {
-                        // Found
-                        ctx.result("found")
-                        ctx.status(500)
-                        return@get
-                    } else {
-                        logger.warn("not expected res")
-                        ctx.result("unexpected")
-                        ctx.status(200)
-                        return@get
-                    }
-                }.onFailure {
-                    logger.error("Ex visiting css: ", it)
-                    ctx.result("err")
-                    ctx.status(200)
-                    return@get
-                }
-            }
+            get("/health", checkCss(cssUrl, ua, jdkHttpClient, logger))
+            head("/health", checkCss(cssUrl, ua, jdkHttpClient, logger))
         }
     }.start("127.0.0.1", 7000)
     Runtime.getRuntime().addShutdownHook(Thread { app.stop() })
+}
+
+private fun checkCss(
+    cssUrl: String,
+    ua: String,
+    jdkHttpClient: HttpClient,
+    logger: Logger
+): (ctx: Context) -> Unit = { ctx ->
+    NaiveRateLimit.requestPerTimeUnit(ctx, 20, TimeUnit.MINUTES)
+    val req = HttpRequest.newBuilder()
+        .uri(URI.create(cssUrl))
+        .header("User-Agent", ua)
+        .build()
+
+    val resFut = jdkHttpClient.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+    runCatching {
+        val res = resFut.get(5, TimeUnit.SECONDS)
+        if (res.body().contains("吃掉了")) {
+            ctx.result("not found yet")
+            ctx.status(200)
+        } else if (res.headers().allValues("content-type").any { it.equals("text/css", true) }) {
+            // Found
+            ctx.result("found")
+            ctx.status(500)
+        } else {
+            logger.warn("not expected res")
+            ctx.result("unexpected")
+            ctx.status(200)
+        }
+    }.onFailure {
+        logger.error("Ex visiting css: ", it)
+        ctx.result("err")
+        ctx.status(200)
+    }
 }
